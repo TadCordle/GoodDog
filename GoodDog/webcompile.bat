81a43af8c7dbbdcb6c41a22838e00c8819b9c17b
Tad Cordle <tad.cordle@gmail.com>
2023-10-23 17:50:14 -0400

    Change last screen to support more keyboards lol oops

--- GoodDog/webcompile.bat
+++ GoodDog/webcompile.bat
@@ -1,1 +1,6 @@
+@echo off
+if exist web\ del web\
+if not exist web\ mkdir web\
 emcc GoodDog.cpp WobblyRender.cpp Game.cpp -s WASM=1 -o web/gooddog.html -D_DEFAULT_SOURCE -Os -I. -I C:/raylib/src -I C:/raylib/src/external -L. -L C:/raylib/src -s USE_GLFW=3 -s ASYNCIFY -s TOTAL_MEMORY=67108864 -s FORCE_FILESYSTEM=1 --preload-file resources/ --shell-file C:/raylib/src/shell.html C:/raylib/src/web/libraylib.a -DPLATFORM_WEB -s EXPORTED_FUNCTIONS=["_free","_malloc","_main"] -s EXPORTED_RUNTIME_METHODS=ccall
+xcopy resources/ web/resources/ /E
+ren web\gooddog.html index.html
